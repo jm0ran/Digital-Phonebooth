271baa9bef0451fc8ad014f13ffce41195f9bbf9
Joseph Moran <josephdeargmoran@protonmail.com>
2022-06-16 17:59:34 -0400

    Hang Up Detection on raspberry Pi

--- Non-Unity/Raspberry Pi Script/phoneProcessingScript.py
+++ Non-Unity/Raspberry Pi Script/phoneProcessingScript.py
@@ -2,10 +2,11 @@
 
 import RPi.GPIO as GPIO #Used to process GPIO inputs and enable power out
 from time import sleep #Used to time the pulse readings
-
 pulse = 32  #GPIO 12, Corresponds to pin for pulse created by dial spin
 gate = 36 #GPIO 16, Corresponds to pin that opens switch when dial is being retracted
+hangup = 40 #GPIO 21, Corresponds to the pin that closes circut when phone is on stand
 gateOpen = False #Default state of gate is false
+phoneLifted = False #Is phone lifted
 
 GPIO.setwarnings(False) # Ignore warning in final version
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
@@ -13,6 +14,7 @@ GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 #Set both recieving pins to PUD_DOWN state
 GPIO.setup(pulse, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(gate, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(hangup, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 NULL_CHAR = chr(0) #Placeholder for empty character
 count = 0 #Staring value for count
@@ -48,6 +50,10 @@ def sendTheMessage(numberPass):
         keyCode = 38
     elif(numberPass == 10):
         keyCode = 39
+    elif(numberPass == 11):
+        keyCode = 11
+    elif(numberPass == 12):
+        keyCode = 15
     else:
         keyCode = 0
     print(numberPass) #Just used for feedback when running while connected to a machine
@@ -58,7 +64,19 @@ def sendTheMessage(numberPass):
 
 #Main loop of code
 while True: # Run indefinitely
+    
+    if(not GPIO.input(hangup) and not phoneLifted):
+        print("Phone lifted")
+        phoneLifted = True
+        sendTheMessage(12)
+        sleep(0.07)
+    if(GPIO.input(hangup) and phoneLifted):
+        print("Phone has been put down")
+        phoneLifted = False
+        sendTheMessage(11)
+        sleep(0.07)
     if(GPIO.input(pulse) == 0): #If pulse closes, this indicates a pulse, runs once for every pulse
+        print("pulse")
         count += 1
         sleep(0.07) #Sleep matches pulse timings to acquire accurate counts
     if(not gateOpen and GPIO.input(gate) == 1): #Initial function to open the gate and begin the counting of pulses
